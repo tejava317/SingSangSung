05d3278ba02bc47ffa955df07abf46f99eed756e
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-12-26 16:29:52 +0900

    initial commit

---
+++ app/src/main/java/com/example/singsangsung/MainActivity.kt
@@ -0,0 +1,11 @@
+tpackage com.example.singsangsung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
